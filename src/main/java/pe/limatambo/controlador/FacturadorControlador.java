acd42f3ee6a0b75f8b3bf346c25143513c220fa2
MarioMario <marioramos083@gmail.com>
2018-03-28 15:03:31 -0500

    master

--- src/main/java/pe/limatambo/controlador/FacturadorControlador.java
+++ src/main/java/pe/limatambo/controlador/FacturadorControlador.java
@@ -6,19 +6,12 @@
 package pe.limatambo.controlador;
 
 import java.io.IOException;
-import java.util.List;
 import javax.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import pe.limatambo.entidades.Categoria;
 import pe.limatambo.excepcion.GeneralException;
-import pe.limatambo.util.Mensaje;
-import pe.limatambo.util.Respuesta;
 
 /**
  *
@@ -31,7 +24,8 @@ public class FacturadorControlador {
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity listar(HttpServletRequest request) throws GeneralException, IOException {
         try {
-            Runtime.getRuntime().exec("cmd /c start D:\\Jetty\\iniciarSistema.bat");
+            Runtime.getRuntime().exec("cmd /c start D:\\SUNAT\\iniciarSistema.bat");
+            Runtime.getRuntime().exec("cmd /c start D:\\SUNAT\\abrirbandeja.bat");
         } catch (IOException ioe) {
             System.out.println(ioe);
         }
